09f662dfee030b86d1991003ba0200ea65528a64
San Shwe <sanxhwe@gmail.com>
2024-05-04 22:09:51 +0630

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from "./components/Hero";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
